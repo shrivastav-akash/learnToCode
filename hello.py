fba0975cf699a4b3c802b8340897947c00c4b5aa
Akash Shrivastav <shrivastav.work@gmail.com>
2025-03-10 10:18:50 +0530

    modified python

--- hello.py
+++ hello.py
@@ -6,9 +6,49 @@ class movie_recommendation_system:
         self.release_date = release_date
         self.genres = genres
 
-    def movie_makers(dir, prod):
-        director = dir
-        production = prod
+    def movie_makers(self, dir, prod):
+        self.director = dir
+        self.production = prod
+        print(f"Director: {self.director},\nProduction of: {self.production}")
 
+    def get_details(self):
+        print(f"Movie Name: {self.name},\nRating: {self.rating},\nRelease date: {self.release_date},\nGenres: {self.genres}")
+
+# movie1 = movie_recommendation_system("DDLJ", 8.5, "23/04/2000", "Rom-Com")
+# movie1.get_details()
+# movie1.movie_makers("Ptta nahi....!", "Dharma Production")
+
+
+# learn about called method and calling method
+class Mother:
+    def setName(self, name):
+        self.name = name
+
+class Kid(Mother):
+    def setMarks(self, marks):
+        self.marks = marks
+
+class Father:
+    def __init__(self, name, age):
+        self.property = 78000
+        self.name = name
+        self.age = age
+
+    def display_property(self):
+        print("father's property:", self.property)
+
+class Son(Father):
+    def __init__(self, age):
+        self.age = age
+
+
+kid1 = Kid()
+print(kid1.setName("Dyuti"))
+print(kid1.setMarks(45))
+print(kid1.name)
+print(kid1.marks)
+
+s = Son()
+s.display_property()
     
     
