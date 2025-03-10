e8e370d3f2e02929457c14ce8110d8656cff8426
Akash Shrivastav <shrivastav.work@gmail.com>
2025-03-10 09:13:19 +0530

    Remove demo.txt and add initial implementation of movie recommendation system in hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+class movie_recommendation_system:
+
+    def __init__(self, name, rating, release_date, genres):
+        self.name = name
+        self.rating = rating
+        self.release_date = release_date
+        self.genres = genres
+
+    def movie_makers(dir, prod):
+        director = dir
+        production = prod
+
+    
+    
